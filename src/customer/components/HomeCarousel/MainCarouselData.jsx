6168c04c41301652f5d311c7cb15556e6a5fd28e
Naina <naina.verma@cleartrip.com>
2024-11-19 21:08:15 +0530

    Added files

---
+++ src/customer/components/HomeCarousel/MainCarouselData.jsx
@@ -0,0 +1,18 @@
+export const mainCaraouselData=[
+    {
+        image:"https://letsdressup.in/cdn/shop/files/TieDye_Banner.jpg?v=1725693206",
+        path:"/women/clothing/short_dress"
+    },
+    {
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDIpUQsdG85gmKH1hbpohRt3TDq7KL1ip8XaoUWuxSGDG8dNVo4G5OmU3wZ3XJMYzLrZ0&usqp=CAU",
+        path:"/women/clothing/lehnga_choli"
+    },
+    {
+        image:"https://letsdressup.in/cdn/shop/collections/5_1500x.png?v=1728039019",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYfBX7RLqhGvyjWcCw_i1TbF5kCVXAMAxUfIfnUa22NP00_cB0HzceWaHbNrV2DAHezH4&usqp=CAU",
+        path:"/women/clothing/women_saree"
+    }
+]
